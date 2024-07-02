5cc92b7bfeb7ffa48f2b1264ecd75cb227d08af8
Azzahraaf <azzahra27407@gmail.com>
2024-07-02 22:08:58 +0700

    config website

--- js/script.js
+++ js/script.js
@@ -5,6 +5,36 @@ function replaceName() {
     document.getElementById("name").innerHTML = name
 }
 
-document.getElementById ("tombol").addEventListener("click", function() {
-    replaceName
+document.getElementById ('tombol').addEventListener("click", function() {
+    replaceName();
 })
+
+
+let nameInput = document.getElementById('name-input')
+
+document.getElementById('kirim').addEventListener("click", function() {
+prompt()
+})
+
+function validateForm() {
+    const name = document.forms["message-form"]["full-name"].value;
+    const birthDate = document.forms["message-form"]["birth-date"].value;
+    const gender = document.forms["message-form"]["gender"].value;
+    const messages = document.forms["message-form"]["messages"].value;
+
+    if (name == "" || birthDate == "" || gender == "" || messages == "") {
+        alert("Tidak boleh ada yang kosong") ;
+        return false;
+        }
+
+    setSenderUI (name, birthDate, gender, messages);
+    return false;
+
+    }
+
+function setSenderUI (name, birthDate, gender, messages) {
+    document.getElementById("sender-full-name").innerHTML = name;
+    document.getElementById("sender-birth-date").innerHTML = birthDate
+    document.getElementById("sender-gender").innerHTML = gender;
+    document.getElementById("sender-messages").innerHTML = messages;
+}
